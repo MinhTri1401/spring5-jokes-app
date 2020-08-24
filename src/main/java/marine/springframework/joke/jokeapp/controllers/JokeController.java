b8417d9324a731fc4e7c052e45fb0cd1c9c6f3ac
MinhTri1401 <minhtri1412000@gmail.com>
2020-08-24 23:18:20 +0700

    Display Joke String properties and fix some typo

--- src/main/java/marine/springframework/joke/jokeapp/controllers/JokeController.java
+++ src/main/java/marine/springframework/joke/jokeapp/controllers/JokeController.java
@@ -17,7 +17,7 @@ public class JokeController {
 
     @RequestMapping({"/",""})
     public String showJoke(Model model){
-        model.addAttribute("jokes",jokeService);
+        model.addAttribute("joke",jokeService.getJoke());
         return "chucknorris";
     }
 }
